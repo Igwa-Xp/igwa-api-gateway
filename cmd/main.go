d0cae72790cb004d44c83b3466dff1d6e12a379f
Duncan-Kiragu <mathenge089@gmail.com>
2023-05-22 18:29:38 +0300

    add entrant file

--- cmd/main.go
+++ cmd/main.go
@@ -1,47 +1,78 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/Igwa-Xp/igwa-api-gateway/pkg/auth"
+// 	"github.com/Igwa-Xp/igwa-api-gateway/pkg/config"
+// 	"github.com/Igwa-Xp/igwa-api-gateway/pkg/experience"
+// 	"github.com/Igwa-Xp/igwa-api-gateway/pkg/organizer"
+// 	// "github.com/Igwa-Xp/igwa-api-gateway/pkg/order"
+// 	// "github.com/Igwa-Xp/igwa-api-gateway/pkg/routes"
+// )
+
+// func main() {
+// 	// Load configuration
+// 	cfg := config.LoadConfig()
+
+// 	// Initialize authentication service client
+// 	authSvc := auth.InitServiceClient(cfg)
+
+// 	// Initialize experience service client
+// 	expSvc := experience.InitServiceClient(cfg)
+
+// 	// Initialize organizer service client
+// 	orgSvc := organizer.InitServiceClient(cfg)
+
+// 	// Initialize order service client
+// 	// orderSvc := order.InitServiceClient(cfg)
+
+// 	// Initialize the Gin router
+// 	router := gin.Default()
+
+// 	// Register routes for each microservice
+// 	auth.RegisterRoutes(router, cfg)
+// 	experience.RegisterRoutes(router, expSvc)
+// 	organizer.RegisterRoutes(router, orgSvc)
+// 	auth.RegisterRoutes(router, cfg, authSvc)
+
+// 	// Start the server
+// 	log.Printf("Server listening on port %s", cfg.Server.Port)
+// 	err := http.ListenAndServe(":"+cfg.Server.Port, router)
+// 	if err != nil {
+// 		log.Fatal("Failed to start server: ", err)
+// 	}
+// }
+
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
+    "github.com/Igwa-Xp/igwa-api-gateway/pkg/auth"
+    "github.com/Igwa-Xp/igwa-api-gateway/pkg/config"
+    "github.com/Igwa-Xp/igwa-api-gateway/pkg/organizer"
+    "github.com/Igwa-Xp/igwa-api-gateway/pkg/experience"
 	"github.com/Igwa-Xp/igwa-api-gateway/pkg/auth"
-	"github.com/Igwa-Xp/igwa-api-gateway/pkg/config"
-	"github.com/Igwa-Xp/igwa-api-gateway/pkg/experience"
-	"github.com/Igwa-Xp/igwa-api-gateway/pkg/organizer"
-	// "github.com/Igwa-Xp/igwa-api-gateway/pkg/order"
-	// "github.com/Igwa-Xp/igwa-api-gateway/pkg/routes"
+
+	
 )
 
 func main() {
-	// Load configuration
-	cfg := config.LoadConfig()
+    c, err := config.LoadConfig()
 
-	// Initialize authentication service client
-	authSvc := auth.InitServiceClient(cfg)
+    if err != nil {
+        log.Fatalln("Failed at config", err)
+    }
 
-	// Initialize experience service client
-	expSvc := experience.InitServiceClient(cfg)
+    r := gin.Default()
 
-	// Initialize organizer service client
-	orgSvc := organizer.InitServiceClient(cfg)
+    authSvc := *auth.RegisterRoutes(r, &c)
+    organizer.RegisterRoutes(r, &c, &authSvc)
+    experience.RegisterRoutes(r, &c, &authSvc)
 
-	// Initialize order service client
-	// orderSvc := order.InitServiceClient(cfg)
-
-	// Initialize the Gin router
-	router := gin.Default()
-
-	// Register routes for each microservice
-	auth.RegisterRoutes(router, cfg)
-	experience.RegisterRoutes(router, expSvc)
-	organizer.RegisterRoutes(router, orgSvc)
-	auth.RegisterRoutes(router, cfg, authSvc)
-
-	// Start the server
-	log.Printf("Server listening on port %s", cfg.Server.Port)
-	err := http.ListenAndServe(":"+cfg.Server.Port, router)
-	if err != nil {
-		log.Fatal("Failed to start server: ", err)
-	}
+    r.Run(c.Port)
 }
